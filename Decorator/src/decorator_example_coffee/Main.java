78e77d2e6d40354e50496b617c3c8fbb895cc09f
Peter Kaiser <pjk@arcor.de>
2018-12-26 00:54:37 +0100

    more coffees

--- Decorator/src/decorator_example_coffee/Main.java
+++ Decorator/src/decorator_example_coffee/Main.java
@@ -3,8 +3,12 @@ package decorator_example_coffee;
 public class Main {
 
 	public static void main(String[] args) {
-		CoffeeVariation c = new WithMilk(new BasicCoffee());
-		System.out.println(c.getDetails());
+		CoffeeVariation c = new BasicCoffee();
+		System.out.print("Black "+c.getDetails());
+		System.out.println(", "+c.getPrice());
+		CoffeeVariation d = new WithMilk(c);
+		System.out.print("With Milk "+d.getDetails());
+		System.out.println(", "+d.getPrice());
 
 	}
 
